1dd02d6a2db5079f913b9c8f67c2500678e08dfc
Peter Byfield <peter.byfield@kraken.tech>
2024-12-25 21:05:24 +0100

    Add exclude_imports method

--- src/import_discovery/mod.rs
+++ src/import_discovery/mod.rs
@@ -90,6 +90,21 @@ impl ImportsInfo {
         Ok(imports_info)
     }
 
+    pub fn package_info(&self) -> &PackageInfo {
+        &self.package_info
+    }
+
+    pub fn exclude_imports(
+        &self,
+        imports: impl IntoIterator<Item = (PackageItemToken, PackageItemToken)>,
+    ) -> Result<Self> {
+        let mut imports_info = self.clone();
+        for (from, to) in imports {
+            imports_info.remove_internal_import(from, to)?;
+        }
+        Ok(imports_info)
+    }
+
     fn initialise_maps(&mut self) -> Result<()> {
         for item in self.package_info.get_all_items() {
             self.internal_imports
@@ -116,6 +131,20 @@ impl ImportsInfo {
         if let Some(metadata) = metadata {
             self.internal_imports_metadata.insert((from, to), metadata);
         }
+        Ok(())
+    }
+
+    fn remove_internal_import(
+        &mut self,
+        from: PackageItemToken,
+        to: PackageItemToken,
+    ) -> Result<()> {
+        self.internal_imports.entry(from).or_default().remove(&to);
+        self.reverse_internal_imports
+            .entry(to)
+            .or_default()
+            .remove(&from);
+        self.internal_imports_metadata.remove(&(from, to));
         Ok(())
     }
 }
@@ -244,6 +273,110 @@ from django.db import models
                     line_number: 2,
                     is_typechecking: false,
                 }
+            }
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_exclude_imports() -> Result<()> {
+        let test_package = testpackage! {
+            "__init__.py" => "
+import testpackage.a
+from testpackage import b
+",
+            "a.py" => "",
+            "b.py" => ""
+        };
+
+        let package_info = PackageInfo::build(test_package.path())?;
+        let imports_info = ImportsInfo::build(package_info)?;
+
+        let root_package = imports_info
+            .package_info
+            .get_item_by_pypath("testpackage")
+            .unwrap()
+            .token();
+        let root_package_init = imports_info
+            .package_info
+            .get_item_by_pypath("testpackage.__init__")
+            .unwrap()
+            .token();
+        let a = imports_info
+            .package_info
+            .get_item_by_pypath("testpackage.a")
+            .unwrap()
+            .token();
+        let b = imports_info
+            .package_info
+            .get_item_by_pypath("testpackage.b")
+            .unwrap()
+            .token();
+
+        assert_eq!(
+            imports_info.internal_imports,
+            hashmap! {
+                root_package => hashset! {root_package_init},
+                root_package_init => hashset! {a, b},
+                a => hashset! {},
+                b => hashset!{},
+            }
+        );
+
+        assert_eq!(
+            imports_info.reverse_internal_imports,
+            hashmap! {
+                root_package => hashset!{},
+                root_package_init => hashset! {root_package},
+                a => hashset! {root_package_init},
+                b => hashset! {root_package_init},
+            }
+        );
+
+        assert_eq!(
+            imports_info.internal_imports_metadata,
+            hashmap! {
+                (root_package_init, a) => ImportMetadata{
+                    line_number: 2,
+                    is_typechecking: false,
+                },
+                (root_package_init, b) => ImportMetadata{
+                    line_number: 3,
+                    is_typechecking: false,
+                },
+            }
+        );
+
+        let imports_info = imports_info.exclude_imports(vec![(root_package_init, a)])?;
+
+        assert_eq!(
+            imports_info.internal_imports,
+            hashmap! {
+                root_package => hashset! {root_package_init},
+                root_package_init => hashset! {b},
+                a => hashset! {},
+                b => hashset!{},
+            }
+        );
+
+        assert_eq!(
+            imports_info.reverse_internal_imports,
+            hashmap! {
+                root_package => hashset!{},
+                root_package_init => hashset! {root_package},
+                a => hashset! {},
+                b => hashset! {root_package_init},
+            }
+        );
+
+        assert_eq!(
+            imports_info.internal_imports_metadata,
+            hashmap! {
+                (root_package_init, b) => ImportMetadata{
+                    line_number: 3,
+                    is_typechecking: false,
+                },
             }
         );
 
